3c67113f07c9a0b15d408e7a6a4fd2f81427be70
Raju S Baradur <rajubaradur24@gmail.com>
2025-04-20 23:08:51 +0530

    file uploded

---
+++ pages/Team.py
@@ -0,0 +1,27 @@
+import streamlit as st
+
+col1, col2, col3,col4= st.columns(4)
+
+# Column 1 - Priyanka M
+with col1:
+    st.markdown("### Priyanka M")
+    st.image(r'C:\Users\Raju\OneDrive\Desktop\Nail_Health-main\images\team\Priyanka M.jpg')
+    col1.markdown("###### GitHub : [Github](https://github.com/Priyankagithub2004)")
+
+# Column 2 - Prajwal M
+with col2:
+    st.markdown("### Prajwal M")
+    st.image(r'C:\Users\Raju\OneDrive\Desktop\Nail_Health-main\images\team\Prajwal.jpg')
+    col2.markdown("###### GitHub : [Github](https://github.com/Prajwal590)")
+
+# Column 3 - Raju S B
+with col3:
+    st.markdown("### Raju S B")
+    st.image(r'C:\Users\Raju\OneDrive\Desktop\Nail_Health-main\images\team\Raju S B.jpg')
+    col3.markdown("###### GitHub : [Github](https://github.com/RAJUS248)")
+
+# Column 4 - Kartik G
+with col4:
+    st.markdown("### Kartik G")
+    st.image(r'C:\Users\Raju\OneDrive\Desktop\Nail_Health-main\images\team\Kartik G.jpg')
+    col4.markdown("###### GitHub : [Github](https://github.com/Kartik-Galgali)")
